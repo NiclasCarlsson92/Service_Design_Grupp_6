b7c80fc08eb5209a4d504461fc38a8c6a5e892a5
estanisarandon <estanislao.sarandon@student.kyh.se>
2022-04-19 12:00:49 +0200

    fisrt

---
+++ CoinMarketCap_API.py
@@ -0,0 +1,34 @@
+
+
+def main():
+    # This example uses Python 2.7 and the python-request library.
+
+    from requests import Request, Session
+    from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+    import json
+
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+    parameters = {
+        'start': '1',
+        'limit': '5000',
+        'convert': 'USD'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        # API Key is linked to an account created by estani
+        'X-CMC_PRO_API_KEY': '12c385ec-6a53-458e-b418-d4a987d2e3a5',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    try:
+        response = session.get(url, params=parameters)
+        data = json.loads(response.text)
+        print(data)
+    except (ConnectionError, Timeout, TooManyRedirects) as e:
+        print(e)
+
+if __name__ == '__main__':
+    main()
+
