0ba155d6a97d91a402ba4b7381690b0c2a125d45
Niclas Carlsson <niclas.carlsson@student.kyh.se>
2022-05-10 08:53:09 +0200

    Changes made to test_app

--- src/test/test_app.py
+++ src/test/test_app.py
@@ -1,7 +1,7 @@
 """tests made for our api"""
 import json
 import pytest
-from app import create_app, db
+from app import create_app
 from controllers.user_controller import get_user_by_id
 from blueprints.api import api_get
 
@@ -39,8 +39,3 @@ def test_coinmarketcap_api(client):
     response = api_get()
     json_data = json.dumps(response)
     assert json_data is not None
-
-
-def test_database_connection(client):
-    """Test to see there's a valid connection to our database"""
-    pass
